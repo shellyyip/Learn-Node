99085faa0fd0eafb604d1b95b9c0cfc773845366
Shelly Yip <shelly@shellyyip.com>
2018-03-04 12:46:36 -0500

    Get & render all stores on /, /stores routes

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -15,3 +15,9 @@ exports.createStore = async (req, res) => {
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`)
 }
+
+exports.getStores = async (req, res) => {
+  // 1. Query db for list of all stores
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores })
+}
