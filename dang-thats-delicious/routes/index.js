b8e5f2ac5d11b39ba3b963cfaa599df057a21525
Shelly Yip <shelly@shellyyip.com>
2018-03-13 17:48:28 -0400

    Set up login & register forms, User model, validateRegister middleware

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
+const userController = require('../controllers/userController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 router.get('/', catchErrors(storeController.getStores))
@@ -23,5 +24,12 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore))
 router.get('/tags', catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 
+router.get('/login', userController.loginForm)
+router.get('/register', userController.registerForm)
+
+// 1. Validate registration data
+// 2. Register user
+// 3. Log user in
+router.post('/register', userController.validateRegister)
 
 module.exports = router;
